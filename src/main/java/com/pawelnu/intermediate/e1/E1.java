12e780c7dbdc02f76aa1271a7c20dc548a98bddb
pawelNu <93542936+pawelNu@users.noreply.github.com>
2024-01-07 12:21:05 +0100

    Intermediate/exercise1 (#1)
    * exercise 1
    
    * code style
    
    * exercise 1 solution
    
    * code refactor

---
+++ src/main/java/com/pawelnu/intermediate/e1/E1.java
@@ -0,0 +1,60 @@
+package com.pawelnu.intermediate.e1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class E1 {
+    /*
+    Create a list of strings (any 10 strings) and write a program
+     which displays the average length of the strings
+     do not contain spaces or capital letters when removed from strings
+     dots and commas.
+    */
+    public static void main(String[] args) {
+        List<String> list = DataExample.getStringListWithAllCombinations();
+        System.out.println(calculateAvgStringLength(list));
+        System.out.println(calculateAvgStringLengthStream(list));
+
+        List<String> list2 = DataExample.getStringListWithEmptyString();
+        System.out.println(calculateAvgStringLength(list2));
+        System.out.println(calculateAvgStringLengthStream(list2));
+    }
+
+    public static Double calculateAvgStringLength(List<String> list) {
+        List<Integer> newList = new ArrayList<>();
+        for (String string : list) {
+            if (!string.contains(" ") && string.compareTo(string.toLowerCase()) == 0) {
+                newList.add(E1.replaceAllDotsAndCommas(string).length());
+            }
+        }
+
+        return calculateAvg(newList);
+    }
+
+    public static Double calculateAvgStringLengthStream(List<String> list) {
+        return list.stream()
+                .filter(s -> !s.contains(" "))
+                .filter(s -> s.compareTo(s.toLowerCase()) == 0)
+                .map(E1::replaceAllDotsAndCommas)
+                .mapToInt(String::length)
+                .average()
+                .orElse(0.0);
+    }
+
+    private static String replaceAllDotsAndCommas(String s) {
+        return s.replaceAll("[.,]", "");
+    }
+
+    private static Double calculateAvg(List<Integer> list) {
+        if (list.isEmpty()) {
+            return 0.0;
+        }
+
+        Integer sum = 0;
+        for (Integer value : list) {
+            sum += value;
+        }
+
+        return ((double) sum) / list.size();
+    }
+}
